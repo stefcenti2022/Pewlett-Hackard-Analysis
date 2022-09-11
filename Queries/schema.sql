cf0e23208a8d97e986e7324cb7a2eeebf32ba009
Stefanie Centi <stefcenti@centium.com>
2022-09-11 17:52:41 -0400

    Modify tables that could not load due to incorrect schema for data in CVS file.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -27,8 +27,8 @@ CREATE TABLE dept_manager(
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -46,18 +46,21 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+-- NOTE: There is no primary key in titles since
+-- an employee can have multiple titles. 
+-- If we want a unique row, we would need a date range to retrieve
+-- the title want for the query.
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title INT NOT NULL,
+  title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  );
 
-SELECT * FROM departments;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
+SELECT * FROM departments LIMIT 10;
+SELECT * FROM dept_emp LIMIT 10;
+SELECT * FROM dept_manager LIMIT 10;
+SELECT * FROM employees LIMIT 10;
+SELECT * FROM salaries LIMIT 10;
+SELECT * FROM titles LIMIT 10;
