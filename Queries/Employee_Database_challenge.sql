b6856f24b27ea4b4fba829581bebe195be6d4ad4
stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>
2022-09-15 01:48:25 -0400

    Update sql to expand eligibility.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -76,4 +76,25 @@ ORDER BY e.emp_no;
 
 SELECT * FROM mentorship_eligibility;
 
+-- Expand the mentorship eligibility requirements to retrieve
+-- enough employees to fill the roles of potential retirees.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO expanded_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (t.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1961-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+select * from expanded_eligibility;
 
