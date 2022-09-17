d441f5987750351cdd787897c6d1976dafc74744
stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>
2022-09-17 19:12:31 -0400

    New query and image for jr level employees that are eligible for retirement

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -76,6 +76,32 @@ ORDER BY e.emp_no;
 
 SELECT * FROM mentorship_eligibility;
 
+-- *** Code for Deliverable 3
+
+--*********************************************
+-- Queries for images shown in Results Section
+--*
+
+-- 1st img with duplicate employees
+select * from retirement_titles;
+
+-- 2nd img w/o duplicate employees
+select * from unique_titles;
+
+-- 3rd img shows the number of roles opening up
+select * from retiring_titles;
+
+-- 4th img shows small pool of mentees
+select * from mentorship_eligibility;
+
+--*********************************************
+-- Queries for images shown in Summary Section
+--*
+
+-- 1st img shows number of roles opening up
+select * from retiring_titles;
+
+-- 2nd img shows larger pool of mentees
 -- Expand the mentorship eligibility requirements to retrieve
 -- enough employees to fill the roles of potential retirees.
 
@@ -85,6 +111,7 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.birth_date,
 	de.from_date,
 	de.to_date,
+	de.dept_no,
 	t.title
 INTO expanded_eligibility
 FROM employees AS e
@@ -98,3 +125,91 @@ ORDER BY e.emp_no;
 
 select * from expanded_eligibility;
 
+
+-- Before performing the final query for display, the
+-- following code was used to create 2 additional tables to
+-- hold employee info for employees with senior titles in one
+-- one table and employee info with junior titles in the other. 
+
+----------------------------------------------------------------------
+-- We want to find a list of employees that are near retirement that
+-- currently have a "Senior" title. These employees will be the first
+-- ones to be contacted to enter the mentorship program as Mentors.
+--
+-- Note the following:
+--
+-- mr = Mentor
+--
+-- unique_titles contains a unique list of employees along with
+-- their current title who are about to retire.
+--
+-- retiring_sr_mentors will be asked to join the first wave of mentors.
+----------------------------------------------------------------------
+SELECT de.dept_no,
+	e.birth_date,
+	mr.title,
+	e.emp_no,
+	e.first_name,
+	e.last_name
+INTO retiring_sr_mentors
+FROM unique_titles AS mr,
+	employees AS e,
+	dept_emp AS de
+WHERE mr.title IN ('Senior Engineer', 'Senior Staff', 'Manager', 'Technique Leader')
+AND mr.emp_no = e.emp_no
+AND de.emp_no = e.emp_no
+AND de.to_date = '9999-01-01'
+ORDER BY de.dept_no, e.birth_date, mr.title;
+
+SELECT * from retiring_sr_mentors;
+
+----------------------------------------------------------------------
+-- Next we want to find a list of employees that are near retirement that
+-- currently have a "Junior" title. These employees will be the first
+-- ones to be contacted to enter the mentorship program as Mentees.
+--
+-- This is for employees that may be eligible for retirement but are
+-- willing and able to take on a more senior role before doing so.
+--
+-- Note the following:
+--
+-- me = Mentee
+--
+-- unique_titles contains a unique list of employees along with
+-- their current title who are about to retire.
+--
+-- retiring_jr_mentees will be asked to join the first wave of mentees.
+----------------------------------------------------------------------
+SELECT de.dept_no,
+	e.birth_date,
+	me.title,
+	e.emp_no,
+	e.first_name,
+	e.last_name
+INTO retiring_jr_mentees
+FROM unique_titles AS me,
+	employees AS e,
+	dept_emp AS de
+WHERE me.title IN ('Engineer', 'Assistant Engineer', 'Staff')
+AND me.emp_no = e.emp_no
+AND de.emp_no = e.emp_no
+AND de.to_date = '9999-01-01'
+ORDER BY de.dept_no, e.birth_date, me.title;
+
+SELECT * from retiring_jr_mentees;
+
+-- 3rd img shows junior level titles about to retire
+
+-------------------------------------------------------------------------
+-- Find all employees with junior roles that are eligible for retirement.
+--
+-- These employees may be interested in becoming a mentee vs mentor
+-- in the mentorship program if they are not ready to retire and are
+-- able and willing to advance.
+--
+-- In order to show a more varied sampling, we selected a small set of
+-- random birthdates.
+-------------------------------------------------------------------------
+SELECT * 
+FROM retiring_jr_mentees 
+WHERE birth_date IN ('1952-01-05', '1953-02-05', '1954-03-05', '1955-04-05');
